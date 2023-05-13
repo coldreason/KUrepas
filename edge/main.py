bbb8fb4f74dbd045b8f5bed3bf730da8c16a81f1
Youtube_Larry_Official <94779160+Clawesome-Larry@users.noreply.github.com>
2023-05-14 01:13:47 +0900

    Update main.py

--- edge/main.py
+++ edge/main.py
@@ -3,6 +3,12 @@ from read_request import *
 from update_designate_queue import *
 from write_score import *
 
+get_taskIDs_with_unitID_zero(requestQueue)
+get_taskIDs_with_unitID_one(requestQueue)
+get_taskIDs_with_unitID_two(requestQueue)
+get_taskIDs_with_unitID_three(requestQueue)
+get_taskIDs_with_unitID_four(requestQueue)
+
 write1_scoreQueue(courier1_score)
 write2_scoreQueue(courier2_score)
 write3_scoreQueue(courier3_score)
