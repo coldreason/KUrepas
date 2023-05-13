018345a688d950b13fbb7b951f72e442844dbc10
Youtube_Larry_Official <94779160+Clawesome-Larry@users.noreply.github.com>
2023-05-14 01:11:51 +0900

    Update main.py

--- edge/main.py
+++ edge/main.py
@@ -3,4 +3,8 @@ from read_request import *
 from update_designate_queue import *
 from write_score import *
 
-def write1_scoreQueue(courier1_score):
+write1_scoreQueue(courier1_score)
+write2_scoreQueue(courier2_score)
+write3_scoreQueue(courier3_score)
+write4_scoreQueue(courier4_score)
+write5_scoreQueue(courier5_score)
