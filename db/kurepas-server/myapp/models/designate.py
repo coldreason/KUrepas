4d32ee2c318fe55367f86f98c08da7c06c787fab
coldreason <coldreason@korea.ac.kr>
2023-05-14 00:51:18 +0900

    change to aws setting

--- db/kurepas-server/myapp/models/designate.py
+++ db/kurepas-server/myapp/models/designate.py
@@ -1,21 +1,23 @@
 from db import db
 from datetime import datetime
 
-
-class Score(db.Model):
+class Designate(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    task_id = db.Column(db.Integer, unique=False, nullable=False)
+    task_id = db.Column(db.Integer, unique=True, nullable=False)
     unit_id = db.Column(db.Integer, unique=False, nullable=False)
     score = db.Column(db.Integer, unique=False, nullable=False)
+    done = db.Column(db.Boolean, unique=False, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def from_json(json_obj):
-        score = Score()
-        score.task_id = json_obj['task_id']
-        score.unit_id = json_obj['unit_id']
-        score.score = json_obj['score']
-        return score
+        designate = Designate()
+        designate.id = json_obj['id']
+        designate.task_id = json_obj['task_id']
+        designate.unit_id = json_obj['unit_id']
+        designate.score = json_obj['score']
+        designate.done = json_obj['done']
+        return designate
     
     def to_json(self):
         return {
@@ -23,6 +25,7 @@ class Score(db.Model):
             "task_id": self.task_id,
             "unit_id": self.unit_id,
             "score": self.score,
+            "done": self.done,
             "created_at": self.created_at.isoformat(),
             "updated_at": self.updated_at.isoformat()
         }
--- db/kurepas-server/myapp/models/requestscore.py
+++ db/kurepas-server/myapp/models/requestscore.py
@@ -1,28 +1,28 @@
 from db import db
 from datetime import datetime
 
-
-class Score(db.Model):
+class Requestscore(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task_id = db.Column(db.Integer, unique=False, nullable=False)
     unit_id = db.Column(db.Integer, unique=False, nullable=False)
-    score = db.Column(db.Integer, unique=False, nullable=False)
+    done = db.Column(db.Boolean, unique=False, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def from_json(json_obj):
-        score = Score()
-        score.task_id = json_obj['task_id']
-        score.unit_id = json_obj['unit_id']
-        score.score = json_obj['score']
-        return score
+        requestscore = Requestscore()
+        requestscore.id = json_obj['id']
+        requestscore.task_id = json_obj['task_id']
+        requestscore.unit_id = json_obj['unit_id']
+        requestscore.done = json_obj['done']
+        return requestscore
     
     def to_json(self):
         return {
             "id": self.id,
             "task_id": self.task_id,
             "unit_id": self.unit_id,
-            "score": self.score,
+            "done": self.done,
             "created_at": self.created_at.isoformat(),
             "updated_at": self.updated_at.isoformat()
         }
